01274dab70579e3a7e606a28db76101132b8aaf3
Collette Curson <ccurson@forgetdata.com>
2016-11-24 11:36:41 +0000

    added example of using format_labels

--- Scripts/transformations/labels/format_labels.py
+++ Scripts/transformations/labels/format_labels.py
@@ -6,11 +6,21 @@ class FormatSettings:
     For example, the label_format will expect a matrix, member, or group.
     The cell_format will expect a Cell.
      
-    The label_format string can contain whatever the matrix/group/member/cell can 
-    format,  for example  label_format = "{0.Group}: {0}"
+    The label_format string can contain whatever the matrix/group/member/cell
+    can format,  for example  label_format = "{0.Group}: {0}"
+    
     The cell_format string can contain whatever the cell can format, eg
     cell_format = "${0[0].Value} XXX"
-     
+    
+    Examples:
+    
+    | import transformations.utils.utilities as utils
+    | m = utils.matrixfuncs.create_test_matrix()
+    | from transformations.labels.format_labels import FormatSettings
+    | settings = FormatSettings(label_format="{0.Member.Label}: " + "{0[0][0].Value}")
+    | print ", ".join([settings.label_format(r) for r in m])
+    | myRow 0, myRow 1, myRow 2, myRow 3, myRow 4
+    
     """
     
     def __init__(self, label_format = "{0}", cell_format="{0}"):
@@ -21,4 +31,4 @@ class FormatSettings:
         if isinstance(cell_format, str):
             self.cell_format = cell_format.format
         elif callable(cell_format):
-            self.cell_format = cell_format
+            self.cell_format = cell_format 
